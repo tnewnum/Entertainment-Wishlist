855ce9feee5ba56a6840ae5aa386832f9993226e
Thomas Newnum <thomas.newnum@gmail.com>
2023-01-12 21:08:20 -0500

    saving the code

--- Assets/marvel.js
+++ Assets/marvel.js
@@ -1,46 +1,63 @@
- function comics () {
- 
- //fetch request for comics from API
- fetch('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=edeef70bcb85a7b1d24cbec22d4ae7d4&hash=4210b3cb502a39ad299a252758911801')
+// let seriesBtn = document.getElementById('#series')
+// let characterBtn = document.getElementById('#characters')
 
- //then function that returns the API call into json
- .then(function (response) {
- return response.json()
- })
- //then it turns the json response into a variable "data"
- .then(function (data) {
-     console.log(data)
 
-    //  let linkBack = data.attributionHTML
-    //  console.log(linkBack)
+function comics () {
 
-    //list of empty arrays established for later use
-    let comics = []
+    let comicsBtn = document.querySelector('#comics')
+    comicsBtn.addEventListener(`click`, () =>{
 
-   
-
-    //This works but I cannot get it to work in the loop
-    //  let images = []
-    //  images.push(data.data.results[3].images[0].path)
-    //  console.log(images)
-
+            //fetch request for comics from API
+            fetch('https://gateway.marvel.com/v1/public/comics?ts=1&apikey=edeef70bcb85a7b1d24cbec22d4ae7d4&hash=4210b3cb502a39ad299a252758911801')
        
-        
+            // /then function that returns the API call into json
+            .then(function (response) {
+            return response.json()
+            })
 
-        for (let i = 0; i < 20; i++) {
-
-            comics.push(data.data.results[i].title)
-            console.log(comics) 
-            
-            // let listGroup = document.querySelectorAll('.list-group')
-
-            // listGroup[i].innertext = comics[i]
-
-        }
+            //then it turns the json response into a variable "data"
+            .then(function (data) {
+                console.log(data)
+       
+                //  let linkBack = data.attributionHTML
+                //  console.log(linkBack)
+       
+                //list of empty arrays established for later use
+               let comics = []
+               let images = []
+          
+       
+              
+               
+       
+               for (let i = 0; i < 20; i++) {
+       
+                   comics.push(data.data.results[i].title)
+                   console.log(comics) 
+       
+                   if ( data.data.results[i].images[0] !== undefined) {
+       
+                       images.push(data.data.results[i].images[0].path)
+                       console.log(images)
+       
+                   }
+       
+       
+              
+                   
+                   // let listGroup = document.querySelectorAll('.list-group')
+       
+                   // listGroup[i].innertext = comics[i]
+       
+               }
+       
+       
+            })
 
 
     })
- }; comics ()
+ 
+}; comics ()
 
 
 //  function characters () {
