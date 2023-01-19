d109967ccc930e16686e7979697fe3d7f8bfea0c
Ryan Mennemeier <kalpha1865@gmail.com>
2023-01-19 13:49:14 -0500

    final update for rawg js, refactor

--- Assets/js/rawg.js
+++ Assets/js/rawg.js
@@ -1,8 +1,8 @@
 
 
 //establish API key and general API call
-
-const apiKey = "f292e982be664ec2994676e290830bc7";
+const key = "f292e982be664ec2994676e290830bc7"
+const apiKey = "https://api.rawg.io/api/platforms?token&key=" + key;
 
 const platformButtonEl = document.querySelectorAll(".bttn")
 console.log(platformButtonEl)
@@ -13,10 +13,10 @@ console.log(platformButtonEl)
   
   
   
-  //write function that calls Rawg API data for all platforms
+  //write function that calls Rawg API data for all platforms (note hardcoding api Key as string literal and apiKey const wasn't working)
   function getAPIdata(){
     const selectedPlatform = this.textContent;
-    fetch('https://api.rawg.io/api/platforms?token&key=f292e982be664ec2994676e290830bc7')
+    fetch(apiKey)
 
     //have it report to console log
       .then(function(response){
@@ -27,21 +27,29 @@ console.log(platformButtonEl)
       
       
     //   single out noted specific consoles based on html class to generate that list    
-      for(let i = 0; i < 20; i++){
+      for(let i = 0; i < data.results.length; i++){
           console.log(data.next)
           if(data.results[i].name == selectedPlatform){
-              let html =""
-              for (let j = 0; j<data.results[i].games.length;j++ ){
-                  html += `
-                  <div class="card game-list" data-id="${data.results[i].games[j].id}" data-gname="${data.results[i].games[j].name}" >
-                  <h3 class="card-header">${data.results[i].games[j].name}</h3>
-                  </div>
-                  `
+
+            let html ="";
+            for (let j = 0; j < data.results[i].games.length; j++ ){
+
+                let gameData = data.results[i].games[j];
+
+                html += '<div class="card game-list" onclick="gameListClick(' + gameData.id + ', ' + gameData.name + ')" data-id="' + gameData.id + '" data-gname="' + gameData.name + '" >'
+                html += '<h3 class="card-header">' + gameData.name + '</h3></div>'
+                  
+                  
+                  
                 }
                 //have list pop in app under Showing Card (repos-container)
                 document.getElementById("repos-container").innerHTML = html;
                 const gameListBTN = document.querySelectorAll(".game-list")
-                gameListBTN.forEach(element => element.addEventListener("click",gameListClick))
+
+                // doing a quick for loop through each button created for each game -- element = i in a for loop
+                gameListBTN.forEach((element) => element.addEventListener("click",gameListClick))
+                
+                
                 break;
             }
         }
@@ -59,6 +67,7 @@ function gameListClick(){
     var gameid = this.getAttribute("data-id")
     var gameName = this.getAttribute("data-gname")
     console.log("Game",gameid)
+    
     var storedGameList = JSON.parse(localStorage.getItem("entertainment")) || []
     storedGameList.push({id:gameid, game:gameName})
     localStorage.setItem("entertainment", JSON.stringify(storedGameList))
